5776e58b266c9cd316773b8621bdbed762b4e149
Ruben Ernst <4404015+Ruben-E@users.noreply.github.com>
2022-01-28 16:14:26 +0100

    Added import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
+import { NextSeo } from "next-seo";
 
 export default function Home() {
   return (
